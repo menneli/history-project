2f219bb26f98ff2723a7e0faa0727d2f3dcd0c40
menneli <131284866+menneli@users.noreply.github.com>
2026-04-16 00:09:13 +0300

    another try

--- config.py
+++ config.py
@@ -3,6 +3,7 @@
 """
 from pathlib import Path
 from pydantic_settings import BaseSettings
+import os
 
 if Path(".env").exists():
     from dotenv import load_dotenv
@@ -17,14 +18,14 @@ class Settings(BaseSettings):
     PROJECT_VERSION: str = "1.0.0"
 
     # Настройки базы данных
-    DATABASE_URL: str = "sqlite:///./data/hs.db"
-
-    # Дополнительные настройки
-    API_V1_STR: str = "/api/v1"
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite:////app/db/hs.db")
 
     @property
     def EXCEL_DATA_PATH(self) -> str:
         return str(Path.cwd() / "data" / "history_sounds.xlsx")
+
+    # Дополнительные настройки
+    API_V1_STR: str = "/api/v1"
 
     model_config = {"env_file": ".env", "env_file_encoding": "utf-8", "extra": "allow"}
 
